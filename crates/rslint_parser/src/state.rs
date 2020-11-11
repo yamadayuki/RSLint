c81451d1ad8aebb801ae57d50200b32a5b05c6c3
rslint <rdambrosio016@gmail.com>
2020-11-11 15:05:40 -0500

    Feat: interfaces

--- crates/rslint_parser/src/state.rs
+++ crates/rslint_parser/src/state.rs
@@ -45,6 +45,7 @@ pub struct ParserState {
     pub in_case_cond: bool,
     pub(crate) no_recovery: bool,
     pub in_declare: bool,
+    pub in_binding_list_for_signature: bool,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -74,6 +75,7 @@ impl Default for ParserState {
             in_case_cond: false,
             no_recovery: false,
             in_declare: false,
+            in_binding_list_for_signature: false,
         }
     }
 }
